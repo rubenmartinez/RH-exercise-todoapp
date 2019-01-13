e5a56c7cac572cc20daf54747a3b590f3e80a27f
rubenmartinez <rubenmarty@gmx.com>
2019-01-13 16:25:58 +0100

    created new submodule just for todo mgmt, just to show 2 'microservices'

---
+++ rhe-todo/src/main/java/net/rubenmartinez/redhat_exercise/rhe_todo/App.java
@@ -0,0 +1,13 @@
+package net.rubenmartinez.redhat_exercise.rhe_todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
